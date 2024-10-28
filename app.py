7708747cb43b542d56fdeecb73ef9c6e865f2219
brayan-1a <i2414619@continental.edu.pe>
2024-10-28 16:25:13 -0500

    Añadir archivo app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@
 import streamlit as st
 
 st.title("Bienvenido a la Aplicación de Mercado")
-st.write("Este es un ejemplo básico de una aplicación Streamlit.")
+st.write("¡Bienvenido a mi aplicación!")
